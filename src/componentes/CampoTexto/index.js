608849f69881f84b95758d77dc5d4f9362b380e4
Gustavo Soares <150816585+gustavoosoares@users.noreply.github.com>
2024-07-19 17:07:43 -0300

    Projeto organo

---
+++ src/componentes/CampoTexto/index.js
@@ -0,0 +1,23 @@
+import './CampoTexto.css';
+
+const CampoTexto = (props) => {
+
+    const aoDigitar = (evento) => {
+        props.aoAlterado(evento.target.value)
+    }
+
+    return (
+        <div className="campo-texto">
+            <label>
+                {props.label}
+            </label>
+            <input 
+             required={props.obrigatorio} 
+             placeholder={props.placeholder}
+             value={props.valor}
+             onChange={aoDigitar} />
+        </div>
+    )
+}
+
+export default CampoTexto;
